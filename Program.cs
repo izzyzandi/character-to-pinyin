49a00b3f096c4b62527193efcca7067f06bc2527
Izzy Zandi <eeyiz1@nottingham.ac.uk>
2025-04-09 19:42:55 +0100

    created functions to convert the pinyin format to a conventional one

--- Program.cs
+++ Program.cs
@@ -19,10 +19,214 @@ namespace character_to_pinyin
             {
                 Console.WriteLine("Enter a Chinese Word");  // have the user input a Chinese word
                 string input_character = Console.ReadLine();              
-                Console.WriteLine(word_dictionary[input_character].pinyin); // print the pinyin
+                Console.WriteLine(Format_pinyin(word_dictionary[input_character].pinyin)); // format and print the pinyin
             }
             
 
+        }
+
+        private static String Format_pinyin(String pinyin_) // take each formatted syllable and combine to one word
+        {
+            String formatted_pinyin = "";
+
+            string[] split_pinyin = pinyin_.Split(new char[] { ' ' });
+            
+            foreach (string syllable in split_pinyin)
+            {
+                String current_formatted_pinyin = " ";
+
+                if (syllable.EndsWith('1'))
+                {
+                    current_formatted_pinyin = pinyin_format_tone1(syllable);
+                }
+                else if (syllable.EndsWith('2'))
+                {
+                    current_formatted_pinyin = pinyin_format_tone2(syllable);
+                }
+                else if (syllable.EndsWith('3'))
+                {
+                    current_formatted_pinyin = pinyin_format_tone3(syllable);
+                }
+                else if (syllable.EndsWith('4'))
+                {
+                    current_formatted_pinyin = pinyin_format_tone4(syllable);
+                }
+                else
+                    current_formatted_pinyin = syllable.Replace('5',' ');
+
+                formatted_pinyin += current_formatted_pinyin;
+            }
+
+            return formatted_pinyin;
+        }
+
+        private static string pinyin_format_tone1(String syllable)  // format tone 1 syllables
+        {
+            string x = syllable.Replace('1', ' ');  // remove the number at the end of the syllable
+            string formatted_syllable = x.Replace('a', 'ā'); // no matter the vowel combination, 'a' has priority for the tone mark
+
+            if (!syllable.Contains('a'))    // if no 'a', 'o' and 'e' are of equal priority and do not feature as a tone pairing so can be checked together
+            {
+                formatted_syllable = formatted_syllable.Replace('o', 'ō');
+                formatted_syllable = formatted_syllable.Replace('e', 'ē');
+
+                if (!syllable.Contains('o') && !syllable.Contains('e')) // if no 'a','e','o', then 'i' and 'u' get the tone mark depending on which is last
+                {
+                    if (syllable.Contains('i') && syllable.Contains('u'))
+                    {
+                        var i_index = formatted_syllable.IndexOf('i');
+                        var u_index = formatted_syllable.IndexOf('u');
+
+                        if (i_index < u_index)
+                        {
+                            formatted_syllable = formatted_syllable.Replace('u', 'ū');
+                        }
+                        else
+                        {
+                            formatted_syllable = formatted_syllable.Replace('i', 'ī');
+                        }
+
+
+                    }
+                    else    // if no 'i','u' tone pairing, then accent the vowel in the syllable
+                    {
+                        formatted_syllable = formatted_syllable.Replace('i', 'ī');
+                        formatted_syllable = formatted_syllable.Replace('u', 'ū');
+                        formatted_syllable = formatted_syllable.Replace('v', 'ü');
+                    }
+
+                }
+
+            }
+
+            return formatted_syllable;
+        }
+
+
+        private static string pinyin_format_tone2(String syllable)  // format tone 2 syllables
+        {
+            string x = syllable.Replace('2', ' ');  // remove the number at the end of the syllable
+            string formatted_syllable = x.Replace('a', 'á'); // no matter the vowel combination, 'a' has priority for the tone mark
+
+            if (!syllable.Contains('a'))    // if no 'a', 'o' and 'e' are of equal priority and do not feature as a tone pairing so can be checked together
+            {
+                formatted_syllable = formatted_syllable.Replace('o', 'ó');
+                formatted_syllable = formatted_syllable.Replace('e', 'é');
+
+                if (!syllable.Contains('o') && !syllable.Contains('e')) // if no 'a','e','o', then 'i' and 'u' get the tone mark depending on which is last
+                {
+                    if (syllable.Contains('i') && syllable.Contains('u'))
+                    {
+                        var i_index = formatted_syllable.IndexOf('i');
+                        var u_index = formatted_syllable.IndexOf('u');
+
+                        if (i_index < u_index)
+                        {
+                            formatted_syllable = formatted_syllable.Replace('u', 'ú');
+                        }
+                        else
+                        {
+                            formatted_syllable = formatted_syllable.Replace('i', 'í');
+                        }
+
+
+                    }
+                    else    // if no 'i','u' tone pairing, then accent the vowel in the syllable
+                    {
+                        formatted_syllable = formatted_syllable.Replace('i', 'í');
+                        formatted_syllable = formatted_syllable.Replace('u', 'ú');
+                        formatted_syllable = formatted_syllable.Replace('v', 'ü');
+                    }
+
+                }
+
+            }
+
+            return formatted_syllable;
+        }
+
+        private static string pinyin_format_tone3(String syllable)  // format tone 3 syllables
+        {
+            string x = syllable.Replace('3', ' ');  // remove the number at the end of the syllable
+            string formatted_syllable = x.Replace('a', 'ǎ'); // no matter the vowel combination, 'a' has priority for the tone mark
+
+            if (!syllable.Contains('a'))    // if no 'a', 'o' and 'e' are of equal priority and do not feature as a tone pairing so can be checked together
+            {
+                formatted_syllable = formatted_syllable.Replace('o', 'ǒ');
+                formatted_syllable = formatted_syllable.Replace('e', 'ě');
+
+                if (!syllable.Contains('o') && !syllable.Contains('e')) // if no 'a','e','o', then 'i' and 'u' get the tone mark depending on which is last
+                {
+                    if (syllable.Contains('i') && syllable.Contains('u'))
+                    {
+                        var i_index = formatted_syllable.IndexOf('i');
+                        var u_index = formatted_syllable.IndexOf('u');
+
+                        if (i_index < u_index)
+                        {
+                            formatted_syllable = formatted_syllable.Replace('u', 'ǔ');
+                        }
+                        else
+                        {
+                            formatted_syllable = formatted_syllable.Replace('i', 'ǐ');
+                        }
+
+
+                    }
+                    else    // if no 'i','u' tone pairing, then accent the vowel in the syllable
+                    {
+                        formatted_syllable = formatted_syllable.Replace('i', 'ǐ');
+                        formatted_syllable = formatted_syllable.Replace('u', 'ǔ');
+                        formatted_syllable = formatted_syllable.Replace('v', 'ǚ');
+                    }
+
+                }
+
+            }
+
+            return formatted_syllable;
+        }
+
+        private static string pinyin_format_tone4(String syllable)  // format tone 4 syllables
+        {
+            string x = syllable.Replace('4', ' ');  // remove the number at the end of the syllable
+            string formatted_syllable = x.Replace('a', 'à'); // no matter the vowel combination, 'a' has priority for the tone mark
+
+            if (!syllable.Contains('a'))    // if no 'a', 'o' and 'e' are of equal priority and do not feature as a tone pairing so can be checked together
+            {
+                formatted_syllable = formatted_syllable.Replace('o', 'ò');
+                formatted_syllable = formatted_syllable.Replace('e', 'è');
+
+                if (!syllable.Contains('o') && !syllable.Contains('e')) // if no 'a','e','o', then 'i' and 'u' get the tone mark depending on which is last
+                {
+                    if (syllable.Contains('i') && syllable.Contains('u'))
+                    {
+                        var i_index = formatted_syllable.IndexOf('i');
+                        var u_index = formatted_syllable.IndexOf('u');
+
+                        if (i_index < u_index)
+                        {
+                            formatted_syllable = formatted_syllable.Replace('u', 'ù');
+                        }
+                        else
+                        {
+                            formatted_syllable = formatted_syllable.Replace('i', 'ì');
+                        }
+
+
+                    }
+                    else    // if no 'i','u' tone pairing, then accent the vowel in the syllable
+                    {
+                        formatted_syllable = formatted_syllable.Replace('i', 'ì');
+                        formatted_syllable = formatted_syllable.Replace('u', 'ù');
+                        formatted_syllable = formatted_syllable.Replace('v', 'ǚ');
+                    }
+
+                }
+
+            }
+
+            return formatted_syllable;
         }
 
         private static IDictionary<string, WordInfo> Populate_dictionary()
